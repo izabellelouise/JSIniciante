3e757eb764ab56f2d0124cef39409c0241cb90b9
Izabelle Louise Nunes de Salles <izabellenunes039@gmail.com>
2020-09-01 17:43:10 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+//DECLARAÇÃO DE VARIÁVEIS
+//criação de uma variável usando o comando var
+//como definir uma variável no JavaScript
+
+var x = 6; 
+
+console.log(x); 
+
+//criação de uma variável usando o comadno const
+const y = "a";  
+//y = "b";
+
+console.log(y);
+{//bloco de código
+  let z = 4;
+
+  console.log(x);
+  console.log(y);
+  console.log(z);
+}
+cosole.log(z);
+
+ 
